d91806d99908499502182bbe2005615d5185a9ac
Karl Johnson <199382398+hirekarl@users.noreply.github.com>
2025-08-06 16:50:00 -0400

    Added interior elements to Todo.

--- context-api-implementation/src/components/Todo.tsx
+++ context-api-implementation/src/components/Todo.tsx
@@ -1,5 +1,16 @@
 export default function Todo() {
   return (
-    <li className="list-group-item">List Item 1</li>
+    <li className="list-group-item">
+      <div className="d-flex justify-content-between align-items-center">
+        <div className="form-check form-check-inline">
+          <input type="checkbox" id="list-item-checkbox" className="form-check-input" />
+          <label htmlFor="list-item-checkbox" className="form-check-label">List Item 1</label>
+        </div>
+        <div className="btn-group">
+          <button className="btn btn-sm btn-warning"><i className="bi bi-pencil-square"></i></button>
+          <button className="btn btn-sm btn-danger"><i className="bi bi-trash"></i></button>
+        </div>
+      </div>
+    </li>
   )
 }
