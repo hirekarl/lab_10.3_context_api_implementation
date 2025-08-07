0a45e99b571007bbdab53dc8f91f000e159741be
Karl Johnson <199382398+hirekarl@users.noreply.github.com>
2025-08-06 23:09:06 -0400

    Fixed typo.

--- context-api-implementation/src/components/Header.tsx
+++ context-api-implementation/src/components/Header.tsx
@@ -2,7 +2,8 @@ import ToggleThemeButton from "./ToggleThemeButton"
 
 export default function Header() {
   return (
-    <div className="d-flex justify-content-between align-items-center my-4">      <h1>Todo App (Context API)</h1>
+    <div className="d-flex justify-content-between align-items-center my-4">
+      <h1>Todo App (Context API)</h1>
       <ToggleThemeButton />
     </div>
   )
